c1333427b4c695ab21a768e76d0630c61683f09d
Issac_R <36460359+Istackz@users.noreply.github.com>
2020-09-14 15:36:51 -0400

    Web Application
    This website is for an appointment booking site for a  local barbershop business. The client books their appointment and can check out styles and contact info of the barber

---
+++ Webpp/test.js
@@ -0,0 +1,8 @@
+function onClickMenu() {
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+// JUNES SRC  src="https://docs.google.com/forms/d/e/1FAIpQLSclVqe5rmzjijw97zu0DcVX7xjFAHeZ0QWbNCCttmBSuMAV0g/viewform?embedded=true"
